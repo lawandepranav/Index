a16875887260b44ed2dab32a57116f0c2b1d1c2b
lawandepranav <lawandepranav@gmail.com>
2024-11-26 09:50:36 +0530

    project initiated

---
+++ $RECYCLE.BIN/S-1-5-21-2836185827-4228487336-1392579167-1001/$RQJDZ6A/.generated/.packager.bat
@@ -0,0 +1,3 @@
+set RCT_METRO_PORT=8081
+set PROJECT_ROOT=D:\codebase\React-native_bluetooth\BluetoothToggleApp
+set REACT_NATIVE_PATH=D:\codebase\React-native_bluetooth\BluetoothToggleApp\node_modules\react-native
